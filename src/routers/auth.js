e2e2fcc35862eaf7fc53290a43a9ecc027f6bb5c
Anton <antonklim6@gmail.com>
2024-07-12 16:21:53 +0200

    reset password

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
-import { registerUserSchema, loginUserSchema } from '../validation/auth.js';
-import { registerUserController, loginUserController, logoutUserController, refreshUserSessionController } from '../controllers/auth.js';
+import { registerUserSchema, loginUserSchema, requestResetEmailSchema } from '../validation/auth.js';
+import { registerUserController, loginUserController, logoutUserController, refreshUserSessionController, requestResetEmailController } from '../controllers/auth.js';
 import validateBody from "../utils/validateBody.js";
 
 const routerAuth = Router();
@@ -10,5 +10,7 @@ routerAuth.post('/register', validateBody(registerUserSchema), ctrlWrapper(regis
 routerAuth.post('/login', validateBody(loginUserSchema), ctrlWrapper(loginUserController));
 routerAuth.post('/logout', ctrlWrapper(logoutUserController));
 routerAuth.post('/refresh', ctrlWrapper(refreshUserSessionController));
+// routerAuth.post('/send-reset-email', );
+routerAuth.post('/request-reset-email', validateBody(requestResetEmailSchema), ctrlWrapper(requestResetEmailController));
 
 export default routerAuth;
